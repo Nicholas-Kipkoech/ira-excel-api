8e3f11d382d5173e4f8bf5e9bcf41088d31566a1
Nicholas Kipkoech <nickey968@gmail.com>
2024-10-16 10:52:16 +0300

    add desc on console

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
-app.listen(8000, () => console.log("server started"));
+app.listen(8000, () => console.log("server started on port 8000"));
 
 app.use("/ira", IRApremRouter);
